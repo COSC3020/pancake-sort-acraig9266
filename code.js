fbd68bcc9da174b02f1b203206a448979c65c639
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-09-26 13:59:05 -0600

    Update code.js
    Implemented flip function

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
 function flip(array, n) {
+    n -= 1;                            // don't know why but have to lower n by 1 before loop instantiation
+    for (i = 0; i < n / 2; i++) {
+        tmp = array[n-i];
+        array[n - i] = array[i];
+        array[i] = tmp;
+    }
     return array;
 }
 
@@ -6,3 +12,7 @@ function flip(array, n) {
 function pancakeSort(array) {
     return array;
 }
+
+
+//a = [1, 2, 3, 4, 5];
+//console.log(flip(a, 5));
